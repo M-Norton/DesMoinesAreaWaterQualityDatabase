e505b4319b02db7b894e4fe616598f57340998ff
Matt Norton <norton.matthew.r@gmail.com>
2020-12-02 13:31:48 -0600

    Add project files.

---
+++ DesMoinesAreaWaterQualityDatabase/DesMoinesAreaWaterQualityDatabase.cpp
@@ -0,0 +1,75 @@
+// DesMoinesAreaWaterQualityDatabase.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+    //Title graphic
+    std::cout << R"(                                                 
+  ____  __  __    _    ____ ____ 
+ |  _ \|  \/  |  / \  / ___/ ___|
+ | | | | |\/| | / _ \| |  | |    
+ | |_| | |  | |/ ___ \ |__| |___ 
+ |____/|_|  |_/_/   \_\____\____|
+                                 
+EGR 155 - Engineering C/C++
+Dev Team:   Hang Thang
+            Taegen Duncan
+            Matt Norton
+
+Instructor: Dr. Edris Ebrahimzadeh
+
+
+ A Des Moines Area Water Quality Database    
+
+=========================================
+=========================================                                                
+)" << '\n';
+
+    //Hard code a 2D array database, data source: DMACC CHM165 class fall 2020: 
+
+
+
+
+    // Populate 2D array with calulated values for Avg.PH; St.Dev; Acidity:
+
+
+
+
+    //Ask user if they would like to lookup water acidity (Y/N):
+
+
+
+
+    //Ask user to enter a water acidity sample source location (choose from list?):
+
+
+
+
+    //Display Avg.PH; St.Dev; and Acidiy of user chosen water source:
+
+
+
+
+    //Ask user if they would like to lookup water hardness (Y/N):
+
+
+
+    //Ask user to enter a water hardness sample source location (choose from list?):
+
+
+
+
+    //Display ppm, and St.Dev of user chosen water source:
+
+
+
+
+
+    return 0;
+}//end main
